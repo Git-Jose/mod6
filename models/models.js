9a1ca29b96bff1c2273c295e495a5b252025b17c
Jose A. Carrascosa Garcia <josecursillos@gmail.com>
2015-06-19 20:41:51 +0200

    Lista de preguntas

--- models/models.js
+++ models/models.js
@@ -1,30 +1,29 @@
 var path = require('path');
 
- //Postgres DATABASE_URL = postgres://user:passwd@host:port/database
- //SQLite   DATABASE_URL = sqlite://:@:/
- var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
- var DB_name  = (url[6] || null);
- var user     = (url[2] || null);
- var pwd      = (url[3] || null);
- var protocol = (url[1] || null);
- var dialect  = (url[1] || null);
- var port     = (url[5] || null);
- var host     = (url[4] || null);
- var storage  = process.env.DATABASE_STORAGE;
- 
+//Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+//SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
 //Cargar modelo ORM
 var Sequelize = require('sequelize');
 
 //Usar Base de datos postgres o sqlite
-var sequelize = new Sequelize(DB_name, user, pwd,{
+var sequelize = new Sequelize(DB_name, user, pwd, {
 	dialect: protocol,
 	protocol: protocol,
 	port: port,
 	host: host,
 	storage: storage, //Sólo SQLite (.env)
-	omitNull: true  //Solo postgres
-	}
-);
+	omitNull: true //Solo postgres
+});
 
 //Usar base de datos SQLite
 //var sequelize = new Sequelize(null, null, null, {
@@ -39,14 +38,18 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz;
 
 //sequelize.sync() crea e inicializa la tabla de preguntas en la base de datos
-sequelize.sync().success(function() { //success(...) ejecuta el manejador una vez creada la tabla, success es la forma antigua
-	Quiz.count().success(function(count) {
+sequelize.sync().then(function() { //then(...) ejecuta el manejador una vez creada la tabla, success es la forma antigua
+	Quiz.count().then(function(count) {
 		if (count === 0) { //la tabla sólo se inicializa si está vacia
 			Quiz.create({
-					pregunta: "Capital de Italia",
-					respuesta: "Roma"
+				pregunta: "Capital de Italia",
+				respuesta: "Roma"
+			});
+			Quiz.create({
+					pregunta: "Capital de Portugal",
+					respuesta: "Lisboa"
 				})
-				.success(function() {
+				.then(function() {
 					console.log("Base de datos inicializada")
 				});
 		}
