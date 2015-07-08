8e3332bc056acf80d73e74a61ce03f98b0c5597c
Jose A. Carrascosa Garcia <josecursillos@gmail.com>
2015-07-08 09:47:44 +0200

    Publicados/no publicados

--- controllers/statistics_controller.js
+++ controllers/statistics_controller.js
@@ -4,7 +4,9 @@ var statistics = {
 	comentarios: 0,
 	media: 0,
 	noComent: 0,
-	siComent: 0
+	siComent: 0,
+	siPubli: 0,
+	noPubli: 0
 };
 
 exports.buscar = function(req, res, next) {
@@ -13,6 +15,15 @@ exports.buscar = function(req, res, next) {
 		models.Comment.count().then(function(count) {
 				statistics.comentarios = count || 0;
 			}),
+			models.Comment.aggregate('publicado', 'count', {  //Contar los publicados
+				distinct: false,
+				where: {
+					publicado: true
+				}
+			}).then(function(count) {
+				statistics.siPubli = count;
+				statistics.noPubli = statistics.comentarios - statistics.siPubli;
+			}),
 			models.Comment.aggregate('QuizId', 'count', {
 				distinct: true
 			}).then(function(count) {
@@ -20,7 +31,9 @@ exports.buscar = function(req, res, next) {
 				statistics.noComent = statistics.preguntas - statistics.siComent;
 				statistics.media = statistics.comentarios / statistics.preguntas;
 				next();
-			})
+			}).catch(function(error) {
+				next(error)
+			});
 	});
 };
 
